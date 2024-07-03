ca6abfa96637cf0cd9bce8441d5036662fe65ede
Qamar/dev <chaudhryqamar.17@gmail.com>
2024-07-03 20:59:53 +0500

    webm file of explosion gif

--- src/components/utils/JuusExplosionVideo.jsx
+++ src/components/utils/JuusExplosionVideo.jsx
@@ -10,12 +10,15 @@ const JuusExplosionVideo = memo(() => {
       viewport={{ once: true }}
       autoPlay
       loop
+      muted
+      playsInline
       className="h-72 lg:h-96 2xl:h-[480px] 3xl:h-[540px] 4xl:h-[640px] 5xl:h-[720px]"
       rel="preload"
+      preload="auto"
     >
       <source
         src={
-          "https://juusstorage.blob.core.windows.net/creatives/Juus%20Home/bottleExplosionJuusPage.mp4"
+          "https://juusstorage.blob.core.windows.net/creatives/Juus%20Home/explosionJuusBottles.webm"
         }
         type="video/mp4"
       />
