e96a4414f770e244d43bb5571a7ec018df7d5d3f
Qamar/dev <chaudhryqamar.17@gmail.com>
2024-08-31 16:30:34 +0500

    testing overflow issue

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import AnimatedRoutes from "./components/utils/AnimatedRoutes.jsx";
 import ScrollToTop from "./components/utils/ScrollToTop.jsx";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
+import { useLocation } from "react-router-dom";
 
 function App() {
+  const {pathname} = useLocation();
   return (
     <Provider store={store}>
-      <div className="relative font-jost">
+      <div className={`${pathname === "/" && "overflow-hidden max-h-screen"} relative font-jost`}>
         <ScrollToTop />
         <AnimatedRoutes />
       </div>
