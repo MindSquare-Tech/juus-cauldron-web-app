d8829b340b178346443d12279a003f67f0bd3e4f
Qamar/dev <chaudhryqamar.17@gmail.com>
2024-08-31 17:13:08 +0500

    faqs page new one

--- src/components/utils/NavLinks.jsx
+++ src/components/utils/NavLinks.jsx
@@ -30,7 +30,7 @@ function NavLinks() {
           Shop
         </h1>
       </Link>
-      <Link to={"https://juus.pk/apps/faqs"}>
+      <Link to={"/faqs"}>
         <h1 className="my-4 lg:my-5 select-none text-snow text-lg lg:text-sm cursor-pointer lg:pt-1.5 lg:px-3.5 hover:underline-offset-4 active:underline hover:underline">
           FAQs
         </h1>
